ffadc0116af87d6ea889adc4b7c7eca258259507
Axel <axosar@gmail.com>
2019-11-15 18:08:51 -0600

    Layout panel

--- src/components/Panel/Panel.js
+++ src/components/Panel/Panel.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 class Panel extends Component {
   constructor(props) {
@@ -7,9 +10,16 @@ class Panel extends Component {
   }
   render() { 
     return (
-      <div>
-        PANEL
-      </div>
+      <Container fluid={true} style={{height: '80vh'}}>
+        <Row style={{height: '100%'}}>
+          <Col xs={12} md={1} style={{background: '#dedede',height: '100%'}}>
+            
+          </Col>
+          <Col xs={12} md={11}>
+            
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
